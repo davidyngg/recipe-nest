f3b59f95367538890a22de95de289dc95fafdb12
davidyngg <yangdavid070@gmail.com>
2026-07-23 21:59:23 -0500

    WIP: Progress on UI showing recipe details
    The recipe detail at this stage has elements to show an image, the recipe
    name and the tags associated with it.

---
+++ views/IngredientTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  IngredientTableViewCell.swift
+//  
+//
+//  Created by David Yang on 7/23/26.
+//
+
+import UIKit
+
+class IngredientTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
