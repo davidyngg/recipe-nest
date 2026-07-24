42e541ad50f8c02074cb1f087cb4698e232c81be
davidyngg <yangdavid070@gmail.com>
2026-07-24 02:41:31 -0500

    fix: Loose source files are now wrapped as an XCode Project

---
+++ recipe-nest/views/RecipeHeaderViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RecipeHeaderViewCell.swift
+//  
+//
+//  Created by David Yang on 7/23/26.
+//
+
+import UIKit
+
+class RecipeHeaderViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
